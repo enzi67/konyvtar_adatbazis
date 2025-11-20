e09c222b0c343f77588ffb40df85c6a461c705d6
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-11-20 14:19:49 +0100

    olvasok

--- konyvtar_adatbazis.sql
+++ konyvtar_adatbazis.sql
@@ -1,7 +1,12 @@
--- 2. feladat --
-CREATE DATABASE konyvtaradatbazis
-CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+bleCREATE DATABASE konyvtaradatbazis CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
 
+USE konyvtaradatbazis;
+
+CREATE TABLE felhasznalok (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nev VARCHAR(255) NOT NULL,
+    regisztracio_idopontja TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+ta
 -- 3. feladat --
 USE konyvtaradatbazis;
 
@@ -11,7 +16,17 @@ CREATE TABLE konyvek (
     cim VARCHAR(200) NOT NULL UNIQUE,
     szerzo VARCHAR(120),
     kiadasi_ev YEAR,
-    oldalszam INTEGER CHECK(BETWEEN 20 AND 2000),
-    kategoria ENUM("fantasy", "sci-fi", "disztópia", "történelem"),
+    oldalszam INTEGER CHECK (oldalszam BETWEEN 20 AND 2000),
+    kategoria ENUM ('fantasy', 'sci-fi', 'disztópia', 'történelem'),
     ertekeles FLOAT DEFAULT 5.0
+);
+
+-- 5. feladat --
+CREATE TABLE olvasok(
+    id INTEGER UNIQUE AUTO_INCREMENT NOT NULL,
+    nev NOT NULL VARCHAR(100),
+    szuletesi_datum DATE,
+    varos VARCHAR(50) DEFAULT 'Budapest',
+    aktiv DEFAULT 'igen' ENUM ('igen', 'nem'),
+    regisztracio_idopontja 
 )
