92b19997e623340535f2787e2d27ad81537d6550
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-11-20 13:53:38 +0100

    létrehozás

--- konyvtar_adatbazis.sql
+++ konyvtar_adatbazis.sql
@@ -1,1 +1,17 @@
-CREATE DATABASE konyvtaradatbazis CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+-- 2. feladat --
+CREATE DATABASE konyvtaradatbazis
+CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+
+-- 3. feladat --
+USE konyvtaradatbazis;
+
+-- 4. feladat --
+CREATE TABLE konyvek (
+    Azonosító INTEGER PRIMARY KEY AUTO_INCREMENT,
+    Cím VARCHAR(200) NOT NULL UNIQUE,
+    Szerző VARCHAR(120),
+    Kiadási év YEAR,
+    Oldalszám INTEGER CHECK(BETWEEN 20 AND 2000),
+    Kategória ENUM("fantasy, "sci-fi, "disztópia, "történelem),
+    Értékelés FLOAT DEFAULT 5.0
+)
