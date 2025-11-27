d3e92b803cd6f54a202767051a3371077cebd10e
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-11-27 13:54:08 +0100

    modositasok

--- videoteka_adatbazis.sql
+++ videoteka_adatbazis.sql
@@ -1,6 +1,6 @@
 -- 2. feladat --
 DROP DATABASE IF EXISTS videotekaadatbazis;
-CREATE database videotekaadatbazis;
+CREATE database videotekaadatbazis
     character set utf8mb4
     collate utf8mb4_hungarian_ci;
 
@@ -11,21 +11,21 @@ USE videotekaadatbazis;
 DROP TABLE IF EXISTS filmek;
 CREATE TABLE filmek (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    cim NOT NULL UNIQUE VARCHAR(150),
+    cim VARCHAR(150) NOT NULL UNIQUE,
     rendezo VARCHAR(120),
     megjelenesi_ev YEAR,
     hossz_perc INT CHECK (hossz_perc BETWEEN 30 AND 400),
     mufaj ENUM('akcio', 'vigjatek', 'drama', 'thriller') NOT NULL,
-    imdb_ertekeles FLOAT DEFAULT 6.5,
+    imdb_ertekeles FLOAT DEFAULT 6.5
 );
 
 -- 5. feladat --
 DROP TABLE IF EXISTS kolcsonzok;
 CREATE TABLE kolcsonzok (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nev NOT NULL VARCHAR(100),
-    email NOT NULL UNIQUE VARCHAR(120),
+    nev VARCHAR(100) NOT NULL,
+    email VARCHAR(120) NOT NULL UNIQUE,
     varos VARCHAR(50) DEFAULT 'Budapest',
-    aktiv ENUM('igen', 'nem') DEFAULT('igen'),
+    aktiv ENUM('igen', 'nem') DEFAULT 'igen',
     regisztracio DATETIME DEFAULT CURRENT_TIMESTAMP
 );
